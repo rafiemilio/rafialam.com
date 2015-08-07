7192272a6c8132ea6f1f865df0242d9c24bacc14
Rafi Alam <Home@rafi.local>
2015-08-07 17:13:20 -0400

    MobileNav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,20 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+var $document = $(document),
+    $element = $('#some-element'),
+    className = 'hasScrolled';
+
+$document.scroll(function() {
+  if ($document.scrollTop() >= 170) {
+    // user scrolled 50 pixels or more;
+    // do stuff
+
+    if ($( window ).width() < 950) {
+    	$("#mobilebar").css("display", "block");
+    }
+    
+    $element.addClass(className);
+  } else {
+  	$("#mobilebar").css("display", "none");
+    $element.removeClass(className);
+  }
+});
